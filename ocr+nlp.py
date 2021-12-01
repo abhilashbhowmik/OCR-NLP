0cf98757950c9df4a42fa49506066ddfcebdb381
NCMohit <mohitnc18@gmail.com>
2021-12-01 07:29:56 +0530

    Fixed typo

--- ocr+nlp.py
+++ ocr+nlp.py
@@ -37,7 +37,7 @@ rep = dict((re.escape(k), v) for k, v in rep.items())
 pattern = re.compile("|".join(rep.keys()))
 text = pattern.sub(lambda m: rep[re.escape(m.group(0))], text)
 
-Using minimum edit distance
+#Using minimum edit distance
 
 print("\nMinimum edit distance result: ")
 for word in text.split():
